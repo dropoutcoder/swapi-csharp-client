1d30d5f81c2119051e697abca4e6df08c08400de
Petr Šrámek <me@dropoutcoder.com>
2019-08-13 18:41:54 +0200

    Added language enum

---
+++ src/DropoutCoder.Swapi/Globalization/Language.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropoutCoder.Swapi.Globalization {
+    public enum Language {
+        English = 0,
+        Wookie = 1
+    }
+}
